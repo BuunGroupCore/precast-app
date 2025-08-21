63c03bd0aa40543ffa49fc93900e3c4fce04e71d
Sacha Roussakis-Notter <sacha.roussakis-notter@buungroup.com>
2025-08-21 11:42:06 +1000

    removed test

--- packages/cli/test-stripe/apps/web/src/components/home/index.ts
+++
@@ -1,4 +0,0 @@
-export { CTA } from "./CTA";
-export { Hero } from "./Hero";
-export { GettingStarted } from "./GettingStarted";
-export { TechnologyStack } from "./TechnologyStack";
--- packages/cli/test-stripe/packages/shared/src/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all shared types, utilities, and constants
-export * from "./types/index";
-export * from "./constants";
-export * from "./utils/index";
-export * from "./schemas/index";
